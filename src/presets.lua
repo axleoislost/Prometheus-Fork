b2088c73c4baa9a3ffe36e1c5c0e010e1bdb3187
axleoislost <jorgeitokramain@gmail.com>
2025-07-24 13:40:43 +0200

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -1,123 +1,115 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
---
--- pipeline.lua
---
--- This Script Provides some configuration presets
+--[[
+    This Script is Part of the Prometheus Obfuscator by Levno_710
+    ---------------------------------------------------------------
+    File: pipeline.lua
+
+    This script provides configuration presets for the obfuscation process.
+--]]
 
 return {
-    ["Strong"] = { -- VERY Strong obfuscation
-        -- The default LuaVersion is Lua51
-        LuaVersion = "LuaU";
-        -- For minifying no VarNamePrefix is applied
-        VarNamePrefix = "";
-        -- Name Generator for Variables that look like this: IlI1lI1l
-        NameGenerator = "MangledShuffled";
-        -- No pretty printing
-        PrettyPrint = false;
-        -- Seed is generated based on current time
-        Seed = 0;
+    ["Strong"] = { -- VERY strong obfuscation preset
+
+        -- Target Lua version
+        LuaVersion = "LuaU",
+
+        -- Variable name prefix (none for minified)
+        VarNamePrefix = "",
+
+        -- Variable name generator (obfuscated names like IlI1lI1l)
+        NameGenerator = "MangledShuffled",
+
+        -- Disable pretty printing for compact output
+        PrettyPrint = false,
+
+        -- Seed is generated based on current time (0 = random)
+        Seed = 0,
+
         -- Obfuscation steps
         Steps = {
             {
-                Name = "Vmify";
-                Settings = {
-                    
-                };
+                Name = "Vmify",
+                Settings = {}
             },
-			{
-				Name = "WatermarkCheck";
-				Settings = {
-					Content = "Prometheus Obfuscator"
-				};
-			},
             {
-                Name = "EncryptStrings";
+                Name = "WatermarkCheck",
                 Settings = {
+                    Content = "Prometheus Obfuscator"
                 }
             },
             {
-                Name = "AntiTamper";
-                Settings = {
-                    UseDebug = false;
-                };
+                Name = "EncryptStrings",
+                Settings = {}
             },
             {
-                Name = "NumbersToExpressions";
+                Name = "AntiTamper",
                 Settings = {
-                    
-                };
-            },
-            {
-                Name = "Vmify";
-                Settings = {
-                    
-                };
-            },
-            {
-                Name = "ConstantArray";
-                Settings = {
-                    Treshold    = 1;
-                    StringsOnly = true;
-                    Shuffle     = true;
-                    Rotate      = true;
-                    LocalWrapperTreshold = 0;
+                    UseDebug = false
                 }
             },
+            {
+                Name = "NumbersToExpressions",
+                Settings = {}
+            },
+            {
+                Name = "Vmify",
+                Settings = {}
+            },
+            {
+                Name = "ConstantArray",
+                Settings = {
+                    Treshold               = 1,
+                    StringsOnly           = true,
+                    Shuffle               = true,
+                    Rotate                = true,
+                    LocalWrapperTreshold  = 0
+                }
+            }
         }
     },
-	
-    ["Normal"] = {
-        -- The default LuaVersion is Lua51
-        LuaVersion = "LuaU";
-        -- For minifying no VarNamePrefix is applied
-        VarNamePrefix = "";
-        -- Name Generator for Variables that look like this: IlI1lI1l
-        NameGenerator = "MangledShuffled";
-        -- No pretty printing
-        PrettyPrint = false;
-        -- Seed is generated based on current time
-        Seed = 0;
-        -- Obfuscation steps
+
+    ["Normal"] = { -- Normal obfuscation preset
+
+        LuaVersion = "LuaU",
+        VarNamePrefix = "",
+        NameGenerator = "MangledShuffled",
+        PrettyPrint = false,
+        Seed = 0,
+
         Steps = {
-			{
-				Name = "WatermarkCheck";
-				Settings = {
-					Content = "Prometheus Obfuscator"
-				};
-			},
             {
-                Name = "EncryptStrings";
+                Name = "WatermarkCheck",
                 Settings = {
+                    Content = "Prometheus Obfuscator"
                 }
             },
             {
-                Name = "AntiTamper";
-                Settings = {
-                    UseDebug = false;
-                };
+                Name = "EncryptStrings",
+                Settings = {}
             },
             {
-                Name = "NumbersToExpressions";
+                Name = "AntiTamper",
                 Settings = {
-                    
-                };
-            },
-            {
-                Name = "Vmify";
-                Settings = {
-                    
-                };
-            },
-            {
-                Name = "ConstantArray";
-                Settings = {
-                    Treshold    = 1;
-                    StringsOnly = true;
-                    Shuffle     = true;
-                    Rotate      = true;
-                    LocalWrapperTreshold = 0;
+                    UseDebug = false
                 }
             },
+            {
+                Name = "NumbersToExpressions",
+                Settings = {}
+            },
+            {
+                Name = "Vmify",
+                Settings = {}
+            },
+            {
+                Name = "ConstantArray",
+                Settings = {
+                    Treshold               = 1,
+                    StringsOnly           = true,
+                    Shuffle               = true,
+                    Rotate                = true,
+                    LocalWrapperTreshold  = 0
+                }
+            }
         }
-    },
+    }
 }
